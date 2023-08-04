bc4ee877b9a15d975a4a81913a9738005bbfafea
kizin_d <dumuch@yandex.ru>
2023-08-04 16:35:56 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+
+    <div class="wrapper">
+        opop[op[
+    </div>
+  </header>
+
+  <main>
+  </main>
+    op[op[
+</template>
+
+<style scoped lang="scss">
+
+</style>
