c23583320391a7ccbdc6a75cda69471609da3c29
kizin_d <dumuch@yandex.ru>
2023-08-09 14:20:32 +0800

    change store types

--- src/stores/weatherWidgetStore.ts
+++ src/stores/weatherWidgetStore.ts
@@ -1,81 +1,88 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { City, AddCity } from '@/models/WeatherWidgetModel';
 
 import { Api } from '@/service/api';
 import type { WeatherData } from '@/models/WeatherWidgetModel';
 import { LocalStorage } from '@/service/localStorage';
+import type { WeatherWidgetStoreState } from '@/stores/weatherWidgetStore.types';
 
 const CITY_LIST_KEY = 'cityList'
 const api = new Api('https://api.openweathermap.org/data/2.5/weather?appid=' + import.meta.env.VITE_OPENWEATHERMAP_ORG_KEY);
 
-export const useWeatherWidgetStore = defineStore('weatherWidget', () => {
-    const cityList = ref<City[]>([]);
+export const useWeatherWidgetStore = defineStore('weatherWidget', {
+        state: (): WeatherWidgetStoreState => ({
+            cityList: []
+        }),
+        actions: {
+            async addCity(newCity: AddCity): Promise<void> {
+                if (!this.cityList.find(city => city.id === newCity.id)) {
+                    this.cityList.push({
+                        id: newCity.id,
+                        lat: newCity.lat,
+                        lng: newCity.lng,
+                        name: newCity.name,
+                        weatherData: null
+                    });
+                    await this.fetchWeather()
+                    this.updateStorage()
+                }
+            },
+            deleteCity(id: number): void {
+                this.cityList = this.cityList.filter(city => city.id !== id)
+                this.updateStorage()
+            },
 
-    async function addCity(newCity: AddCity) {
-        if (!cityList.value.find(city => city.id === newCity.id)) {
-            cityList.value.push({ id: newCity.id, lat: newCity.lat, lng: newCity.lng, name: newCity.name, weatherData: null });
-            await fetchWeather()
-            updateStorage()
-        }
-    }
+            getCityFromLocalStorage(): void {
+                const cities = LocalStorage.getStorage<City[]>(CITY_LIST_KEY)
+                if (cities) {
+                    this.cityList = cities
+                }
+            },
 
-    function deleteCity(id: number) {
-        cityList.value = cityList.value.filter(city => city.id !== id)
-        updateStorage()
-    }
+            async findCityByCityName(cityName: string): Promise<WeatherData> {
+                try {
+                    return await api.get('', { q: cityName });
+                } catch (err) {
+                    throw Error()
+                }
+            },
 
-    function getCityFromLocalStorage() {
-        const cities = LocalStorage.getStorage<City[]>(CITY_LIST_KEY)
-        if (cities) {
-            cityList.value = cities
-        }
-    }
+            updateStorage(): void {
+                LocalStorage.setStorage(CITY_LIST_KEY, this.cityList);
+            },
 
-    async function findCityByCityName(cityName: string): Promise<WeatherData> {
-        try {
-            return await api.get('', { q: cityName });
-        } catch (err) {
-            throw Error()
-        }
-    }
+            async fetchWeather(): Promise<void> {
+                const promises: WeatherData[] = []
 
-    function updateStorage() {
-        LocalStorage.setStorage(CITY_LIST_KEY, cityList.value);
-    }
+                for (const city of this.cityList) {
+                    promises.push(
+                        await api.get('', { id: city.id })
+                    )
+                }
 
-    async function fetchWeather() {
-        const promises:WeatherData[] = []
+                const citiesWeather = await Promise.all<WeatherData>(promises)
 
-        for (const city of cityList.value) {
-            promises.push(
-                await api.get('', { id: city.id })
-            )
-        }
+                this.cityList = this.cityList.map(city => {
+                    const weather = citiesWeather.find(cityWeather => cityWeather.id === city.id)
+                    if (weather) {
+                        city.weatherData = weather
+                    }
+                    return city
+                })
+                this.updateStorage()
+            },
 
-        const citiesWeather = await Promise.all<WeatherData>(promises)
+            newOrder(array: City[]): void {
+                this.cityList = array
+                this.updateStorage()
+            },
 
-        cityList.value = cityList.value.map(city => {
-            const weather = citiesWeather.find(cityWeather => cityWeather.id === city.id)
-            if (weather) {
-                city.weatherData = weather
+            async addCityByLatLng(lat: number, lng: number): Promise<void> {
+                const data = await api.get('', { lat: lat, lon: lng }) as WeatherData;
+                if (data) {
+                    await this.addCity({ id: data.id, lat: lat, lng: lng, name: data.name })
+                }
             }
-            return city
-        })
-        updateStorage()
-    }
-
-    function newOrder(array: City[]) {
-        cityList.value = array
-        updateStorage()
-    }
-
-    async function addCityByLatLng(lat: number, lng: number) {
-        const data = await api.get('', { lat: lat,  lon: lng }) as WeatherData;
-        if (data) {
-            await addCity({ id: data.id, lat: lat, lng: lng, name: data.name })
         }
     }
-
-    return { cityList, addCity, deleteCity, getCityFromLocalStorage, findCityByCityName, fetchWeather, newOrder, addCityByLatLng }
-})
+)
---
+++ src/stores/weatherWidgetStore.types.ts
@@ -0,0 +1,5 @@
+import type { City } from '@/models/WeatherWidgetModel';
+
+export interface WeatherWidgetStoreState {
+    cityList: City[];
+}
